a1ecc34ec4ffa3f7f30efc03b7709eaf59d9129d
Kyler Long <kylerlong24@gmail.com>
2025-05-24 13:36:27 -0400

    Add useState variables for name, email, password, and login mode. Render content dynamically based on loginMode boolean value.

--- portal/src/app/auth/AuthForm.js
+++ portal/src/app/auth/AuthForm.js
@@ -6,25 +6,47 @@ import Button from "@components/Button";
 
 export default function AuthForm() {
 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoginMode, setIsLoginMode] = useState(false);
+
+    //handleSubmit async function
+
     return (
         <div>
-            <h1>Sign Up</h1>
+            <h1>{isLoginMode ? "Login" : "Sign Up"}</h1>
             <p>Let's get started on your project today</p>
             <form>
-                <TextInput
-                type="text"
-                placeholder="Name"
-                />
+                {!isLoginMode && (
+                    <TextInput
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    />
+                )}
                 <TextInput 
                 type="email"
                 placeholder="Email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
                 />
                 <TextInput 
                 type="password"
                 placeholder="Password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
                 />
-                <Button type="submit">Sign Up</Button>
-                <p>Already have an account? <a>Log in</a></p>
+                <Button type="submit">{isLoginMode ? "Login" : "Sign Up"}</Button>
+                <p>
+                    {isLoginMode ? "Need an account?" : "Already have an account?"}
+                
+                    <a onClick={() => setIsLoginMode(!isLoginMode)}
+                    >
+                        {isLoginMode ? "Register Now" : "Login Now"}
+                    </a>
+                </p>
             </form>
             <p>By signing up to create an account I accept Company's <a>Terms of Use and Privacy Policy</a>.</p>
         </div>
